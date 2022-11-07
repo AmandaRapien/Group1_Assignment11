cef70d0081d351ee283dbcc15e5b5cbe38e1d725
grace <grace@GraceDell>
2022-11-07 15:46:48 -0500

    First commit

--- MainPackage/Main.py
+++ MainPackage/Main.py
@@ -15,6 +15,7 @@ import requests #Add with pop
 
 #This is our personal API Key     
 response = requests.get('https://api.weatherbit.io/v2.0/current?city=Cincinati&country=United%20States&key=839131a2085f49389bff0cee0a8460fb')
+#API KEY: 839131a2085f49389bff0cee0a8460fb
 json_string = response.content
     
 parsed_json = json.loads(json_string) # Now we have a python dictionary
@@ -22,9 +23,11 @@ parsed_json = json.loads(json_string) # Now we have a python dictionary
 
     
 for City in parsed_json['data']:         #Get the value associated with paresed_json['data']
-#   print (City)  #this has a list of aspects of the weather
+    #print (City)  #this has a list of aspects of the weather
     print("City Name :", City["city_name"])
     print("Country Name :", City["country_code"])
     print("Date and Time :", City["datetime"])
     print("Sunrise Time :", City["sunrise"])
-    #Will need to print out each aspect indivually 
+    print("Sunset Time :", City["sunset"])
+    print("Temperature :", City["temp"])
+    #Will need to print out each aspect individually 
